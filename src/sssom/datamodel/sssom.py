6ea3e451a3c8bd9c458310b328a813a3a1c4c321
GitHub Action <action@github.com>
2022-04-16 07:35:40 +0000

    Rebuilding documentation and generated files with Github Action

--- src/sssom/datamodel/sssom.py
+++ src/sssom/datamodel/sssom.py
@@ -1,5 +1,5 @@
 # Auto generated from sssom.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-04-15T23:45:20
+# Generation date: 2022-04-16T07:35:35
 # Schema: sssom
 #
 # id: http://w3id.org/sssom/schema/
